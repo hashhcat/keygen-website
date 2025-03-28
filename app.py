e7e47b2af90a2fcf9018d85325cb1fbe5589fbd6
hashcat <kubikmoroz@gmail.com>
2025-03-28 22:58:15 +0200

    Update app.py to use PORT environment variable

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def healthz():
     return jsonify({'status': 'ok'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
